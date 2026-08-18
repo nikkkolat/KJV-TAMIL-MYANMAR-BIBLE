13baf5c401275a5381564c520d43cb3e68047fb1
nikkkolat <76514831+nikkkolat@users.noreply.github.com>
2026-08-18 22:24:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-﻿self.addEventListener('install', () => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', () => {
-    // Do not intercept requests.
-    // GitHub Pages will serve the latest files normally.
-});
+﻿
